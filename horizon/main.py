a7560c3bf95253b92f2e2e843526b431ee9e8f33
Negou  Donald <tameguedonald@gmail.com>
2026-05-20 16:43:57 +0100

    a

--- horizon/main.py
+++ horizon/main.py
@@ -54,7 +54,7 @@ app.add_middleware(HTTPSEnforcementMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3010", "https://horizon.enspy.cm"],
+    allow_origins=["http://localhost:3010", "https://horizon.enspy.cm", "http://192.168.123.100:3010"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
